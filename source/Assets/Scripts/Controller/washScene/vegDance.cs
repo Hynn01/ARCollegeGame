c42eb40900952d29a1ca19f97ecea546d859f3fd
Hynn01 <hynn0633@163.com>
2019-04-09 09:54:14 +0800

    更新洗菜场景

---
+++ source/Assets/Scripts/Controller/washScene/vegDance.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class vegDance : MonoBehaviour
+{
+    Vector3 pos;
+    Renderer rd;
+
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+        pos = this.transform.localPosition;
+        rd = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnClick()
+    {
+        while (Time.deltaTime < 360)
+        {
+            this.transform.position += new Vector3(0 + 5 * Mathf.Cos(Time.deltaTime * 3.14f / 180), 0 + 5 * Mathf.Sin(Time.deltaTime * 3.14f / 180), 0);
+        }
+
+        //让菜回到它原来的位置
+        this.transform.position = pos;
+
+        //让菜变暗
+        var c = rd.material.color;
+        c.a = 0.5f;
+        rd.material.color = c;
+
+    }
+}
---
+++ source/Assets/Scripts/Controller/washScene/washVeg.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class washVeg : MonoBehaviour
+{
+    public GameObject[] vegs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        float y = -5.75f;
+        float x = Random.Range(-6.5f, 6.5f); //随机蔬菜出现的x坐标
+        for(int i=0;i < vegs.Length; i++) {
+            GameObject veg = Instantiate(vegs[i]) as GameObject;
+            veg.transform.position = new Vector3(x, y, 0);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
